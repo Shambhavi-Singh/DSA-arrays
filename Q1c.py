340cba1e886a89223598f500ace4d1b342a98dee
Shambhavi <shambhavis20@gmail.com>
2021-07-17 11:02:04 +0530

    Reverse array using python shortcut slicing method

---
+++ Q1c.py
@@ -0,0 +1,10 @@
+#Python shortcut method to reverse array
+arrOdd=[1,2,3,4,5]
+arrEven=[1,2,3,4]
+print("arrOdd before reversing: ",arrOdd);
+print("arrEven before reversing: ",arrEven);
+
+arrOdd=arrOdd[::-1]
+arrEven=arrEven[::-1]
+print("arrOdd after reversing: ",arrOdd);
+print("arrEven after reversing: ",arrEven);
